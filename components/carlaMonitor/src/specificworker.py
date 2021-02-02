bc810b5e8fa341db7312ed19e8c99176661d9b6f
Araceli <aracelivegamagro@gmail.com>
2021-02-02 11:41:49 +0100

    updated carlaMonitor

--- components/carlaMonitor/src/specificworker.py
+++ components/carlaMonitor/src/specificworker.py
@@ -18,22 +18,29 @@
 #    You should have received a copy of the GNU General Public License
 #    along with RoboComp.  If not, see <http://www.gnu.org/licenses/>.
 #
+import time
 
 from PySide2.QtCore import QTimer
 from PySide2.QtWidgets import QApplication
 from genericworker import *
-
+from multiprocessing import SimpleQueue
+import numpy as np
+import cv2
 
 # If RoboComp was compiled with Python bindings you can use InnerModel in Python
 # sys.path.append('/opt/robocomp/lib')
 # import librobocomp_qmat
-# import librobocomp_osgviewer
+# import librobocomp_osgviewerser
 # import librobocomp_innermodel
 
 class SpecificWorker(GenericWorker):
     def __init__(self, proxy_map, startup_check=False):
         super(SpecificWorker, self).__init__(proxy_map)
-        self.Period = 2000
+        self.Period = 0
+        self.data_queue = SimpleQueue()
+        self.contFPS = 0
+        self.start = time.time()
+
         if startup_check:
             self.startup_check()
         else:
@@ -51,24 +58,22 @@ class SpecificWorker(GenericWorker):
         #	print("Error reading config params")
         return True
 
+    def show_img(self, im):
+        array = np.frombuffer(im.image, dtype=np.dtype("uint8"))
+        array = np.reshape(array, (im.height, im.width, 4))
+        array = array[:, :, :3]
+        window_name = "camera"+str(im.cameraID)
+        cv2.imshow(window_name, array)
+        cv2.waitKey(1)
 
     @QtCore.Slot()
     def compute(self):
-        print('SpecificWorker.compute...')
-        # computeCODE
-        # try:
-        #   self.differentialrobot_proxy.setSpeedBase(100, 0)
-        # except Ice.Exception as e:
-        #   traceback.print_exc()
-        #   print(e)
-
-        # The API of python-innermodel is not exactly the same as the C++ version
-        # self.innermodel.updateTransformValues('head_rot_tilt_pose', 0, 0, 0, 1.3, 0, 0)
-        # z = librobocomp_qmat.QVec(3,0)
-        # r = self.innermodel.transform('rgbd', z, 'laser')
-        # r.printvector('d')
-        # print(r[0], r[1], r[2])
-
+        pass
+        # print('compute')
+        try:
+            self.show_img(self.data_queue.get())
+        except Exception as e:
+            print(e)
         return True
 
     def startup_check(self):
@@ -82,12 +87,13 @@ class SpecificWorker(GenericWorker):
     # SUBSCRIPTION to pushRGBD method from CameraRGBDSimplePub interface
     #
     def CameraRGBDSimplePub_pushRGBD(self, im, dep):
-    
-        #
-        # write your CODE here
-        #
-        pass
+        if time.time() - self.start > 1:
+            print("FPS:", self.contFPS)
+            self.start = time.time()
+            self.contFPS = 0
+        self.contFPS += 1
 
+        self.data_queue.put((im))
 
     # ===================================================================
     # ===================================================================
