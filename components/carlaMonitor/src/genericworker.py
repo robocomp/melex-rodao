22ed00da85b900a64b5c71baa6d2a9bf8a87e44b
Araceli <aracelivegamagro@gmail.com>
2021-02-11 09:05:31 +0100

    carlaMonitor regenerated to provide qt interface

--- components/carlaMonitor/src/genericworker.py
+++ components/carlaMonitor/src/genericworker.py
@@ -78,15 +78,25 @@ setattr(RoboCompCameraRGBDSimple, "DepthType", DepthType)
 import camerargbdsimplepubI
 
 
+try:
+    from ui_mainUI import *
+except:
+    print("Can't import UI file. Did you run 'make'?")
+    sys.exit(-1)
 
 
-class GenericWorker(QtCore.QObject):
+
+class GenericWorker(QtWidgets.QMainWindow):
 
     kill = QtCore.Signal()
 
     def __init__(self, mprx):
         super(GenericWorker, self).__init__()
 
+
+        self.ui = Ui_guiDlg()
+        self.ui.setupUi(self)
+        self.show()
 
         self.mutex = QtCore.QMutex(QtCore.QMutex.Recursive)
         self.Period = 30
