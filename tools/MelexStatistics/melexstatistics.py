c685a4e16bd9a11e8b081e0ffe75fee1e59be2a7
Araceli <aracelivegamagro@gmail.com>
2021-03-04 17:45:13 +0100

    created class FPSPlotWidget

--- tools/MelexStatistics/melexstatistics.py
+++ tools/MelexStatistics/melexstatistics.py
@@ -14,6 +14,61 @@ from pyqtgraph import GraphicsLayoutWidget, DateAxisItem
 RESULTS_DIR = '/home/robocomp/robocomp/components/melex-rodao/files/results/'
 
 
+class FPSPlotWidget(pg.GraphicsLayoutWidget):
+    def __init__(self, file_dir):
+        super(FPSPlotWidget, self).__init__(show=True)
+        self.df1 = pd.read_csv(os.path.join(file_dir, 'carlaBridge_fps.csv'),
+                               delimiter=';', skiprows=0, low_memory=False)
+
+        self.plot1 = self.addPlot(row=1, col=0)
+        self.data1 = self.df1['FPS'].to_numpy()
+        self.label = pg.LabelItem(justify='right')
+        self.addItem(self.label, row=0, col=0)
+
+        self.plot1.plot(self.df1['Time'], self.data1, pen="r")
+
+        axis = pg.DateAxisItem()
+        self.plot1.setAxisItems({'bottom': axis})
+
+        self.region = pg.LinearRegionItem()
+        self.region.setZValue(10)
+        self.region.setRegion([self.df1['Time'][0], self.df1['Time'].iloc[-1]])
+
+        # cross hair
+        self.vLine = pg.InfiniteLine(angle=90, movable=False)
+        self.hLine = pg.InfiniteLine(angle=0, movable=False)
+        self.plot1.addItem(self.vLine, ignoreBounds=True)
+        self.plot1.addItem(self.hLine, ignoreBounds=True)
+
+        self.proxy = pg.SignalProxy(self.plot1.scene().sigMouseMoved, rateLimit=60, slot=self.mouseMoved)
+
+
+    def mouseMoved(self, evt):
+        vb = self.plot1.vb
+        pos = evt[0]  ## using signal proxy turns original arguments into a tuple
+        if self.plot1.sceneBoundingRect().contains(pos):
+            mousePoint = vb.mapSceneToView(pos)
+            xpoint = mousePoint.x()
+
+            nearest1 = self.find_nearest(self.df1['Time'], xpoint)
+            index1 = self.df1.index[self.df1['Time'] == nearest1].tolist()[0]
+
+            datetime_ = datetime.fromtimestamp(mousePoint.x()).strftime("%H:%M:%S")
+
+            self.label.setText(
+                "<span style='font-size: 12pt'>x=%s,   <span style='color: red'>FPS=%0.1f</span>" % (
+                    datetime_, self.data1[index1]))
+
+            self.vLine.setPos(mousePoint.x())
+            self.hLine.setPos(mousePoint.y())
+
+
+    def find_nearest(self, array, value):
+        array = np.asarray(array)
+        idx = (np.abs(array - value)).argmin()
+        return array[idx]
+
+
 class VelocityPlotWidget(pg.GraphicsLayoutWidget):
     def __init__(self, file_dir):
         super(VelocityPlotWidget, self).__init__(show=True)
