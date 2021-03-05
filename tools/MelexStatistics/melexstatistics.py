ae50a858e7620b52ac09007ee50e390e781f39df
Esteban Martinena Guerrero <orensbruli@gmail.com>
2021-03-05 08:42:42 +0100

    [tools] Added more plots to MelexStatistics

--- tools/MelexStatistics/melexstatistics.py
+++ tools/MelexStatistics/melexstatistics.py
@@ -13,150 +13,150 @@ from pyqtgraph import GraphicsLayoutWidget, DateAxisItem
 
 RESULTS_DIR = '/home/robocomp/robocomp/components/melex-rodao/files/results/'
 
+web_colors = ["Red", "Yellow", "Cyan", "Blue", "Magenta"]
+qt_colors = ["r", "y", "g", "b", "m"]
 
-class FPSPlotWidget(pg.GraphicsLayoutWidget):
+
+class AbstractPlotWidget(pg.GraphicsLayoutWidget):
+    def __init__(self):
+        super(AbstractPlotWidget, self).__init__(show=True)
+        self.plots = {}
+        self._current_color = 0
+        self.x_axis_name = ""
+        self.plot_area = self.addPlot(row=1, col=0)
+        self.label = pg.LabelItem(justify='right')
+        self.addItem(self.label, row=0, col=0)
+        # cross hair
+        self.vLine = pg.InfiniteLine(angle=90, movable=False)
+        self.hLine = pg.InfiniteLine(angle=0, movable=False)
+        self.plot_area.addItem(self.vLine, ignoreBounds=True)
+        self.plot_area.addItem(self.hLine, ignoreBounds=True)
+
+        self.proxy = pg.SignalProxy(self.plot_area.scene().sigMouseMoved, rateLimit=60, slot=self.mouseMoved)
+
+        axis = pg.DateAxisItem()
+        self.plot_area.setAxisItems({'bottom': axis})
+
+        self.proxy = pg.SignalProxy(self.plot_area.scene().sigMouseMoved, rateLimit=60, slot=self.mouseMoved)
+
+    def mouseMoved(self, evt):
+        vb = self.plot_area.vb
+        pos = evt[0]  ## using signal proxy turns original arguments into a tuple
+        if self.plot_area.sceneBoundingRect().contains(pos):
+            mousePoint = vb.mapSceneToView(pos)
+            xpoint = mousePoint.x()
+            data_frames_string = ""
+            for plot_index, [plot_name, plot_values] in enumerate(self.plots.items()):
+                next_color = web_colors[plot_index % len(web_colors)]
+                nearest = self.find_nearest(plot_values.xData, xpoint)
+                index = np.where(plot_values.xData == nearest)[0][0]
+                data_frames_string += f", <span style='color: {next_color}'>{plot_name}={plot_values.yData[index]:02.02f}</span>"
+
+            datetime_ = datetime.fromtimestamp(mousePoint.x()).strftime("%H:%M:%S")
+
+            self.label.setText(
+                f"<span style='font-size: 12pt'>x={datetime_}{data_frames_string}")
+            self.vLine.setPos(mousePoint.x())
+            self.hLine.setPos(mousePoint.y())
+
+    def find_nearest(self, array, value):
+        array = np.asarray(array)
+        idx = (np.abs(array - value)).argmin()
+        return array[idx]
+
+    def plot(self, x_axis_name, x_axis_data, y_axis_name, y_axis_data):
+        self.plots[y_axis_name] = self.plot_area.plot(x_axis_data, y_axis_data, pen=qt_colors[self._current_color])
+        self._current_color += 1
+        self.x_axis_name = x_axis_name
+
+
+class FPSPlotWidget(AbstractPlotWidget):
     def __init__(self, file_dir):
-        super(FPSPlotWidget, self).__init__(show=True)
+        super(FPSPlotWidget, self).__init__()
         self.df1 = pd.read_csv(os.path.join(file_dir, 'carlaBridge_fps.csv'),
                                delimiter=';', skiprows=0, low_memory=False)
 
-        self.plot1 = self.addPlot(row=1, col=0)
         self.data1 = self.df1['FPS'].to_numpy()
-        self.label = pg.LabelItem(justify='right')
-        self.addItem(self.label, row=0, col=0)
 
-        self.plot1.plot(self.df1['Time'], self.data1, pen="r")
-
-        axis = pg.DateAxisItem()
-        self.plot1.setAxisItems({'bottom': axis})
-
-        self.region = pg.LinearRegionItem()
-        self.region.setZValue(10)
-        self.region.setRegion([self.df1['Time'][0], self.df1['Time'].iloc[-1]])
-
-        # cross hair
-        self.vLine = pg.InfiniteLine(angle=90, movable=False)
-        self.hLine = pg.InfiniteLine(angle=0, movable=False)
-        self.plot1.addItem(self.vLine, ignoreBounds=True)
-        self.plot1.addItem(self.hLine, ignoreBounds=True)
-
-        self.proxy = pg.SignalProxy(self.plot1.scene().sigMouseMoved, rateLimit=60, slot=self.mouseMoved)
+        self.plot('time', self.df1['Time'], 'fps', self.data1)
 
 
-    def mouseMoved(self, evt):
-        vb = self.plot1.vb
-        pos = evt[0]  ## using signal proxy turns original arguments into a tuple
-        if self.plot1.sceneBoundingRect().contains(pos):
-            mousePoint = vb.mapSceneToView(pos)
-            xpoint = mousePoint.x()
+class ResponseTimePlotWidget(AbstractPlotWidget):
+    def __init__(self, file_dir, *args):
+        super(ResponseTimePlotWidget, self).__init__()
+        df1 = pd.read_csv(os.path.join(file_dir, 'responsetime.csv'),
+                          delimiter=';', skiprows=0, low_memory=False)
 
-            nearest1 = self.find_nearest(self.df1['Time'], xpoint)
-            index1 = self.df1.index[self.df1['Time'] == nearest1].tolist()[0]
+        df1[["CommunicationTime", "ServerResponseTime", "TotalTime"]] = df1[
+            ["CommunicationTime", "ServerResponseTime", "TotalTime"]].apply(lambda x: x * 1000)
 
-            datetime_ = datetime.fromtimestamp(mousePoint.x()).strftime("%H:%M:%S")
+        data1 = df1['TotalTime'].to_numpy()
+        data2 = df1['CommunicationTime'].to_numpy()
+        data3 = df1['ServerResponseTime'].to_numpy()
 
-            self.label.setText(
-                "<span style='font-size: 12pt'>x=%s,   <span style='color: red'>FPS=%0.1f</span>" % (
-                    datetime_, self.data1[index1]))
-
-            self.vLine.setPos(mousePoint.x())
-            self.hLine.setPos(mousePoint.y())
+        self.plot('time', df1['Time'], 'TotalTime', data1)
+        self.plot('time', df1['Time'], 'CommunicationTime', data2)  # Communication_time
+        self.plot('time', df1['Time'], 'ServerResponseTime', data3)  # Server Response
 
 
-    def find_nearest(self, array, value):
-        array = np.asarray(array)
-        idx = (np.abs(array - value)).argmin()
-        return array[idx]
-
-
-class VelocityPlotWidget(pg.GraphicsLayoutWidget):
-    def __init__(self, file_dir):
-        super(VelocityPlotWidget, self).__init__(show=True)
+class VelocityPlotWidget(AbstractPlotWidget):
+    def __init__(self, file_dir, *args):
+        super(VelocityPlotWidget, self).__init__()
         self.df1 = pd.read_csv(os.path.join(file_dir, 'velocity.csv'),
-                          delimiter=';', skiprows=0, low_memory=False)
+                               delimiter=';', skiprows=0, low_memory=False)
         self.df2 = pd.read_csv(os.path.join(file_dir, 'carlaBridge_velocity.csv'),
-                          delimiter=';', skiprows=0, low_memory=False)
-        self.plot1 = self.addPlot(row=1, col=0)
+                               delimiter=';', skiprows=0, low_memory=False)
         self.data1 = self.df1['prom_velocity'].to_numpy()
         self.data2 = self.df2['Velocity'].to_numpy()
-        self.label = pg.LabelItem(justify='right')
-        self.addItem(self.label,row=0, col=0)
 
-        self.plot1.plot(self.df1['Time'], self.data1, pen="r")
-        self.plot1.plot(self.df2['Time'], self.data2, pen="g")
-        axis = pg.DateAxisItem()
-        self.plot1.setAxisItems({'bottom': axis})
-
-        self.region = pg.LinearRegionItem()
-        self.region.setZValue(10)
-        self.region.setRegion([self.df1['Time'][0], self.df1['Time'].iloc[-1]])
+        self.plot('Time', self.df1['Time'], 'prom_velocity', self.data1)
+        self.plot('Time', self.df2['Time'], 'Velocity', self.data2)
 
 
-        # cross hair
-        self.vLine = pg.InfiniteLine(angle=90, movable=False)
-        self.hLine = pg.InfiniteLine(angle=0, movable=False)
-        self.plot1.addItem(self.vLine, ignoreBounds=True)
-        self.plot1.addItem(self.hLine, ignoreBounds=True)
-
-        self.proxy = pg.SignalProxy(self.plot1.scene().sigMouseMoved, rateLimit=60, slot=self.mouseMoved)
-
-
-    def mouseMoved(self, evt):
-        vb = self.plot1.vb
-        pos = evt[0]  ## using signal proxy turns original arguments into a tuple
-        if self.plot1.sceneBoundingRect().contains(pos):
-            mousePoint = vb.mapSceneToView(pos)
-            xpoint = mousePoint.x()
-
-            nearest1 = self.find_nearest(self.df1['Time'], xpoint)
-            index1 = self.df1.index[self.df1['Time'] == nearest1].tolist()[0]
-
-            nearest2 = self.find_nearest(self.df2['Time'], xpoint)
-            index2 = self.df2.index[self.df2['Time'] == nearest2].tolist()[0]
-
-            datetime_ = datetime.fromtimestamp(mousePoint.x()).strftime("%H:%M:%S")
-
-            self.label.setText(
-                "<span style='font-size: 12pt'>x=%s,   <span style='color: red'>Calculated=%0.1f</span>,   "
-                "<span style='color: green'>Carla=%0.1f</span>" % (
-                    datetime_, self.data1[index1], self.data2[index2]))
-            self.vLine.setPos(mousePoint.x())
-            self.hLine.setPos(mousePoint.y())
-
-
-
-    def find_nearest(self, array, value):
-        array = np.asarray(array)
-        idx = (np.abs(array - value)).argmin()
-        return array[idx]
+dock_type = {
+    "carlaBridge_velocity.csv":
+        {"name": "Velocity",
+         "type": VelocityPlotWidget
+         },
+    "responsetime.csv":
+        {"name": "ResponseTime",
+         "type": ResponseTimePlotWidget
+         },
+    "carlaBridge_fps.csv":
+        {"name": "ResponseTime",
+         "type": FPSPlotWidget
+         }
+}
 
 
 class MelexStatistics(QMainWindow):
     def __init__(self):
         super(MelexStatistics, self).__init__()
         self.docks = {}
-        dialog = QtWidgets.QFileDialog(caption="Select result directory", directory=RESULTS_DIR)
-        dialog.setFileMode(QtWidgets.QFileDialog.DirectoryOnly)
-        dialog.setOption(QtWidgets.QFileDialog.ShowDirsOnly, True)
-        dialog.exec_()
-        for dirpath, dnames, fnames in os.walk(dialog.selectedFiles()[0]):
+
+        # dialog = QtWidgets.QFileDialog(caption="Select result directory", directory=RESULTS_DIR)
+        # dialog.setFileMode(QtWidgets.QFileDialog.DirectoryOnly)
+        # dialog.setOption(QtWidgets.QFileDialog.ShowDirsOnly, True)
+        # dialog.exec_()
+        # for dirpath, dnames, fnames in os.walk(dialog.selectedFiles()[0]):
+        for dirpath, dnames, fnames in os.walk(
+                "/home/robolab/robocomp/components/melex-rodao/files/results/pruebaCampusV4"):
             for f in fnames:
-                if "carlaBridge_velocity" in f:
-                    self.create_velocity_dock(dirpath, f)
+                self.create_dock(dirpath, f)
 
+    def create_dock(self, dirpath, f):
 
-
-    def create_velocity_dock(self, dirpath, f):
-        dock = QtWidgets.QDockWidget("Velocity")
-        plot_widget = VelocityPlotWidget(dirpath)
-        dock.setWidget(plot_widget)
-        dock.setAllowedAreas(QtCore.Qt.TopDockWidgetArea)
-        self.docks[f] = dock
-        self.addDockWidget(QtCore.Qt.TopDockWidgetArea, dock)
+        if f in dock_type:
+            current_type = dock_type[f]
+            dock = QtWidgets.QDockWidget(current_type["name"])
+            plot_widget = current_type["type"](dirpath)
+            dock.setWidget(plot_widget)
+            dock.setAllowedAreas(QtCore.Qt.TopDockWidgetArea)
+            self.docks[f] = dock
+            self.addDockWidget(QtCore.Qt.TopDockWidgetArea, dock)
 
 
 if __name__ == '__main__':
-
     QCoreApplication.setAttribute(Qt.AA_ShareOpenGLContexts)
     app = QApplication(sys.argv)
     window = MelexStatistics()
