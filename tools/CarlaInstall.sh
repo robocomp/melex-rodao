6785066ff81c1da992b6438c80b53d66225cce22
Esteban Martinena Guerrero <orensbruli@gmail.com>
2021-03-15 17:14:39 +0100

    [tools] Updated Carla Install file

--- tools/CarlaInstall.sh
+++ tools/CarlaInstall.sh
@@ -1,5 +1,6 @@
-wget https://carla-releases.s3.eu-west-3.amazonaws.com/Linux/CARLA_0.9.10.tar.gz
-mkdir -p carla
-tar -xvf CARLA_0.9.10.tar.gz -C carla 
-cd carla
+pip3 install numpy ice PySide2 argparse termcolor pygame
+wget https://carla-releases.s3.eu-west-3.amazonaws.com/Linux/CARLA_0.9.11.tar.gz
+mkdir -p ../files/carla
+tar -xvf CARLA_0.9.11.tar.gz -C ../files/carla
+cd ../files/carla
 sh ImportAssets.sh
